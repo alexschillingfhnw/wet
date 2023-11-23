9b7cb354f7100e5e7063dfbc879abce3d2601660
alexander.schilling <alexander.schilling@students.fhnw.ch>
2023-11-23 10:46:54 +0100

    update project overview

--- myportfolio/projects/admin.py
+++ myportfolio/projects/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import Project
 
-admin.site.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('title', 'date')
 
+admin.site.register(Project, ProjectAdmin)
--- myportfolio/projects/models.py
+++ myportfolio/projects/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 from django.db.models import Q
+from django.utils import timezone
 
 class Project(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
     image = models.ImageField(upload_to='projects/images/')
     url = models.URLField(blank=True)
+    date = models.DateTimeField(default=timezone.now)
 
     @classmethod
     def search(cls, query):
